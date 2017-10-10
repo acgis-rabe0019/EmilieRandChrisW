79db961e465935f89155070af7b9bc26bfb752cf
LAPTOP-KV4DTCCF\chris <wenk0003@algonquinlive.com>
2017-10-10 11:42:51 -0400

    minor fixes

--- EmilieRandChrisW/ex6.py
+++ EmilieRandChrisW/ex6.py
@@ -1,16 +1,32 @@
 def isNumeric(testString):
+    """Input test string in: '' format only please."""
     if (type (testString) != type ('')):
         return False
-    if testString[0] == '-':
-       return isNumeric(testString[1:])
+    elif testString.isdigit()== True:
+            return True
+    elif testString[0] == '-':
+       if testString[1:].isdigit():
+            return True
+       else:
+        return False
     else:
-        return testString.isdigit()
+        return False
+
 
 
 def test_isNumeric():
+    #expect true for next 2
+   print isNumeric('23')
+   print isNumeric('-23')
+   #expect false
+   print isNumeric(23)
+   print isNumeric('hello')
+   print isNumeric('-number')
+   print isNumeric('*5')
 
 def main():
     test_isNumeric()
 
 if __name__=='__main__':
     main()
+
